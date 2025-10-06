236dff78f402a6d4c2e94d8bfc1e5d96462e0917
6812022610026 <s6812022610026@email.kmutnb.ac.th>
2025-10-06 15:17:54 +0700

    Add files via upload

---
+++ Sec-1_6812022610026_Pawat.c/Coding-Exame_Sec-1/Sec-1_6812022610026_Pawat.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+  int main() {
+
+
+  int OT , i ;
+  float sum, score ;
+  char name[30]
+  
+  
+
+  
+   printf("***OT and tax calcuiator***");
+   printf("Enter nember of OT days(1-10)");
+   scanf("%d", &OT);
+   printf(":%d\n, &OT");
+   for (i = 1 ; i <= OT; ++i){
+   printf("Enter OT houres of days %d:",i);
+   scanf("%f",&score );
+    sum += score;
+  
+  
+
+    }
+
+
+
+
+
+
+
+
+printf("Enter OT hours for Day");
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
